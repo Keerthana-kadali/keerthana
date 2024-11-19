0540e2e641c2cfd957344a2ea7137e74c34209d2
hp <hp@DESKTOP-K627HAN>
2024-11-19 08:32:28 +0530

    commit7

---
+++ svkp_file1/src/Jr_Developer/KIA_AirBags.java
@@ -0,0 +1,15 @@
+package Jr_Developer;
+
+public class KIA_AirBags {
+	public void disp_AirBags(){
+		System.out.println("All KIA have AirBags");
+	}
+}
+
+	
+		
+		
+
+	
+
+
---
+++ svkp_file1/src/Jr_Developer/KIA_Speed_Limit.java
@@ -0,0 +1,9 @@
+package Jr_Developer;
+
+public class KIA_Speed_Limit {
+	public void disp_Speed_Limit()
+	{ 
+		System.out.println("All KIA have Speed Limit");
+
+}
+}
