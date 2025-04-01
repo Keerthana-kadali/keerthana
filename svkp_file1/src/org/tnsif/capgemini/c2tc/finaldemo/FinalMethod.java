a0f5aed698cd13aac4b49d6ac86fffbae84476b9
hp <hp@DESKTOP-K627HAN>
2025-04-01 15:18:20 +0530

    commit 10

---
+++ svkp_file1/src/org/tnsif/capgemini/c2tc/finaldemo/FinalMethod.java
@@ -0,0 +1,33 @@
+package org.tnsif.capgemini.c2tc.finaldemo;
+class method
+{
+	final public void display()
+	{
+		System.out.println("AI is integrated by keerthana");
+	}
+	public void add(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+}
+//class methodname extends method
+//{
+//public void display()
+//{
+		//System.out.println("AI is integrated by prasanna");
+//}
+//	public void add(int a,int b)
+//{
+	//	System.out.println(a+b);
+		//	}
+		//}
+public class FinalMethod {
+
+	public static void main(String[] args) {
+		method obj=new method();
+        obj.display();
+       obj.add(3, 4);
+	}
+
+}
+
---
+++ svkp_file1/src/org/tnsif/capgemini/c2tc/finaldemo/FinalVariable.java
@@ -0,0 +1,12 @@
+package org.tnsif.capgemini.c2tc.finaldemo;
+
+public class FinalVariable {
+
+	public static void main(String[] args) {
+	final int marks=20;
+	//we cant change the final variable
+	System.out.println(marks);
+
+	}
+
+}
